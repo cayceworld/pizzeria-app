17b30a10faf8e44342b62e9655e6c2752fe0c23a
cayceworld <cayceworld@outlook.com>
2021-12-12 14:00:30 +0100

    Fix accordion

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@
          thisProduct.data = data;
          thisProduct.renderInMenu();
          console.log('new Product:', thisProduct);
+         thisProduct.initAccordion();
       }
       renderInMenu() {
          const thisProduct = this;
@@ -77,30 +78,33 @@
       }
       initAccordion() {
          const thisProduct = this;
-         constol.log(thisProduct);
+
          /* find the clickable trigger (the element that should react to clicking) */
-         const clickableTrigger = document.querySelector(select.menuProduct.clickable);
-         console.log(clickableTrigger);
+
+
+         const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+         console.log(select.menuProduct.clickable);
          /* START: add event listener to clickable trigger on event click */
          clickableTrigger.addEventListener('click', function (event) {
             /* prevent default action for event */
             event.preventDefault();
-            
+
             /* find active product (product that has active class) */
-            
-            const activeProduct = thisProduct.querySelector(classNames.menuProduct.wrapperActive);
+
+
+            const activeProduct = document.querySelector(select.all.menuProductsActive);
             console.log(activeProduct);
             /* if there is active product and it's not thisProduct.element, remove class active from it */
-            if(activeProduct != null ){
-               console.log( 'activeProduct = null' );
-            };
-            
+
+            if (activeProduct != null) {
+               activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+            }
             /* toggle active class on thisProduct.element */
             thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive)
          });
 
       }
-      
+
    }
 
    const app = {
